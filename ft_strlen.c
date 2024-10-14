bbda30554e7bbdcc5b3bd68c063afad2470d157c
Gabriel Cesar Nunes Pezoa <gcesar-n@c2r3p5.42sp.org.br>
2024-10-14 15:01:34 -0300

    committtt

---
+++ ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 14:39:21 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/10/14 14:39:44 by gcesar-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int
+	count = 0;
+	while(str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+int	main()
+{
+	char potato[] = "aaaaaaaaaaa";
+	printf("%d", ft_strlen(potato));
+}
