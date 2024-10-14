c89dfa63c972fce21c015b5ae58aa00e6b4ad9fe
Gabriel Cesar Nunes Pezoa <gcesar-n@c2r3p5.42sp.org.br>
2024-10-14 15:57:29 -0300

    dsadsa

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,28 +6,28 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:48:08 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/14 15:00:34 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:55:42 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	ft_toupper(char	*str)
+char	*ft_toupper(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		str[i] += 32;
+		str[i] -= 32;
 		i++;
 	}
-	return (str[i]);
+	return (str);
 }
 
 int	main()
 {
-	char	potato[] = "aaaaaa";
-	printf("%c", ft_toupper(potato));
+	char	str[] = "AAAAA";
+	printf("%s", ft_toupper(str));
 	return (0);
 }
