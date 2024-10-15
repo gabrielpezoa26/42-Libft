7d1e3f55c4de7f89aa3d2c8531240830063307fc
Gabriel <gabrielpezoa26@gmail.com>
2024-10-14 21:53:21 -0300

    strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 21:31:33 by gabriel           #+#    #+#             */
+/*   Updated: 2024/10/14 21:47:49 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] != '\0') && (s2[i] != '\0'))
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+/*
+int main(void)
+{
+    char potato[] = "aab";
+    char beans[] = "aaa";
+    printf("%d", ft_strncmp(potato, beans));
+    return 0;
+}*/
