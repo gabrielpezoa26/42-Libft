dce928c1d08b3b50f93a84f50b7395bde58a56ba
Gabriel Cesar Nunes Pezoa <gcesar-n@c3r4p3.42sp.org.br>
2024-10-15 17:19:18 -0300

    dasdsad

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 15:54:29 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/15 17:18:57 by gcesar-n         ###   ########.fr       */
+/*   Created: 2024/10/15 17:11:49 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/10/15 17:12:17 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-char	*ft_strchr(const char *str, int search_str)
+char	*ft_strrchr(const char *str, int search_str)
 {
 	int	i;
-
+/// fazer um strlen e dps i - 1
 	i = 0;
-	while ((str[i] != '\0') || (str[i] == search_str))
+	while (str[i] != '\0')
 	{
 		if (str[i] == search_str)
 			return ((char *)&str[i]);
-		i++;
+		i--;
 	}
 	return (NULL);
 }
@@ -32,6 +32,6 @@ int	main()
 	int		letter = '\0';
 	char	*potato = "ddddgabcdef";
 
-	printf("dasdsadsa: %s", ft_strchr(potato, letter));
+	printf("dasdsadsa: %s", ft_strrchr(potato, letter));
 	printf("\nfunc original: %s ", strchr(potato, letter));
 }
