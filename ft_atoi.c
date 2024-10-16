be39531792470dddc297901431f3e121d62bd07b
Gabriel <gabrielpezoa26@gmail.com>
2024-10-15 21:46:41 -0300

    asdsada

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:53:14 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/15 15:53:49 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/15 21:46:12 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int i;
+
+	i = 0;
+	if (*str < 9)
+	{
+		i -= 48;
+		return (i);
+	}
+	return (str[i]);
+}
+
+int	main(void)
+{
+    char	potato[] = "6";
+
+	printf("%d", ft_atoi(potato));
+	printf("\n%d  \n", atoi(potato));
+}
--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:54:29 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/15 21:23:48 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/15 21:30:15 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,18 @@ char	*ft_strchr(const char *str, int search_str)
 			return ((char *)&str[i]);
 		i++;
 	}
+	if (search_str == '\0')
+		return ((char *)&str[i]);
+
 	return (NULL);
 }
 
 int	main()
 {
-	int		letter = 'a';
+	int		letter = '\0';
 	char	*potato = "ddddgabcdef";
 
-	printf("dasdsadsa: %s", ft_strchr(potato, letter));
-	//printf("\nfunc original: %s ", strchr(potato, letter));
+	printf("resultado: %s", ft_strchr(potato, letter));
+	printf("\nfunc original: %s \n", strchr(potato, letter));
+	return (0);
 }
