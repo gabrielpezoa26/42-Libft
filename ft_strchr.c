7300e75f911c3905dfe78ced74f6d46b84351e59
Gabriel <gabrielpezoa26@gmail.com>
2024-10-15 21:24:09 -0300

    dadsad

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:54:29 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/15 17:25:39 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/15 21:23:48 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strchr(const char *str, int search_str)
 	int	i;
 
 	i = 0;
-	while ((str[i] != '\0') || (str[i] == search_str))
+	while (str[i] != '\0') 
 	{
 		if (str[i] == search_str)
 			return ((char *)&str[i]);
@@ -27,11 +27,11 @@ char	*ft_strchr(const char *str, int search_str)
 	return (NULL);
 }
 
-// int	main()
-// {
-// 	int		letter = '\0';
-// 	char	*potato = "ddddgabcdef";
+int	main()
+{
+	int		letter = 'a';
+	char	*potato = "ddddgabcdef";
 
-// 	printf("dasdsadsa: %s", ft_strchr(potato, letter));
-// 	printf("\nfunc original: %s ", strchr(potato, letter));
-// }
+	printf("dasdsadsa: %s", ft_strchr(potato, letter));
+	//printf("\nfunc original: %s ", strchr(potato, letter));
+}
