da9b0bf73cc25f3d8680dee7351af2110d3e5ae0
Gabriel Cesar Nunes Pezoa <gcesar-n@c2r9p3.42sp.org.br>
2024-10-17 17:03:02 -0300

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,33 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:53:14 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/15 21:46:12 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/17 16:58:36 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_atoi(const char *str)
+static int	verify_non_printable(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	if (*str < 9)
+	while(str[i] != '\0')
 	{
-		i -= 48;
+		i++;
 		return (i);
 	}
-	return (str[i]);
+	return (i);
+}
+
+static int	verify_sign(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		if (str[i] < 0)
+			return ('-');
+		i++;
+	}
+	return (1);
+}
+int	ft_atoi(const char *str)
+{
+
+	int	result;
+	int	sign;
+	int	i;
+
+	if(str[0] == '-')
+		sign = -1;
+	while(str[i] != '\0')
+	{
+		result = result * 10 + str[i] + '\0';
+		i++;
+	}
+	return (result * sign);
 }
 
 int	main(void)
 {
-    char	potato[] = "6";
+    char	potato[] = "46";
 
-	printf("%d", ft_atoi(potato));
-	printf("\n%d  \n", atoi(potato));
+	printf("func 42: %d \n", ft_atoi(potato));
+	printf("func original: %d  \n", atoi(potato));
 }
