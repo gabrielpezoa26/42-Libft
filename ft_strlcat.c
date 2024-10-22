ba55d8c6b47a56143f569cbc852132c9c76c2701
Gabriel Cesar Nunes Pezoa <gcesar-n@c3r1p2.42sp.org.br>
2024-10-22 18:55:28 -0300

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,33 +6,44 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 21:57:06 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/22 16:39:32 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:53:33 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	size_dest;
 	size_t	size_src;
-	size_t	qto_cabe;
+	size_t	index;
+	size_t	jindex;
 
-	size_dest = ft_strlen(dst);
-	size_src = ft_strlen(src);
-	qto_cabe = (size_dest + size_src) + 1;
-	if (qto_cabe > dstsize)
-		return (dstsize);
-	ft_memcpy(dst, src, dstsize);
-	qto_cabe = '\0';
+	size_dest = 0;
+	size_src = 0;
+	while (dst[size_dest] && size_dest < size)
+		size_dest++;
+	while (src[size_src])
+		size_src++;
+	if (size_dest >= size)
+		return (size_dest + size_src);
+	index = size_dest;
+	jindex = 0;
+	while (src[jindex] != '\0' && index + 1 < (size))
+	{
+		dst[index] = src[jindex];
+		index++;
+		jindex++;
+	}
+	dst[index] = '\0';
 	return (size_dest + size_src);
 }
 
-int	main()
-{
-	char	potato[] = "aaaaa";
-	char	beanss[] = "aaaac";
-	printf("%zu\n", ft_strlcat(potato, beanss, 10));
-	return (0);
-}
+// int	main()
+// {
+// 	char	potato[] = "aaaaa";
+// 	char	beanss[] = "aaaac";
+// 	printf("%zu\n", ft_strlcat(potato, beanss, 10));
+// 	return (0);
+// }
