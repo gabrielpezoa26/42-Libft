f61d39e469ae7f5c9e68c6187428e92c226d7ea0
Gabriel <gabrielpezoa26@gmail.com>
2024-10-23 22:49:28 -0300

    calloccccccc

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,6 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 11:51:55 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/10/23 22:46:21 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
 #include "libft.h"
 
-void *ft_calloc(size_t nitems, size_t size)
+void	*ft_calloc(size_t nitems, size_t size)
 {
-	
+	void	*beans;
+	size_t	potato;
+
+	potato = nitems * size;
+	beans = malloc(potato);
+	if (beans == NULL)
+		return (NULL);
+	ft_memset(beans, 0, potato);
+	return (beans);
 }
+
+/*int main() 
+{
+    int *ptr = (int *)ft_calloc(5, sizeof(int));
+    free(ptr);
+    return 0;
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:01:40 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/23 20:27:51 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/23 22:47:03 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *nptr);
-void	*ft_calloc(size_t num, size_t size);
+void	*ft_calloc(size_t nitems, size_t size);
 char	*ft_strdup(char *src);
 
 #endif
