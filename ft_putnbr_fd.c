ad17dcf99f1d9f031ceb497d2e9bb34d14746307
Gabriel Cesar Nunes Pezoa <gcesar-n@c3r1p2.42sp.org.br>
2024-10-25 13:01:51 -0300

    putnbrrr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:46:09 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/24 21:52:53 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/25 11:49:32 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_putnbr_fd(int n, int fd)
 	if (nb < 0)
 	{
 		write(fd, "-", 1);
-		nb *= -1;
+		nb = -nb;
 	}
 	if (nb > 9)
 	{
