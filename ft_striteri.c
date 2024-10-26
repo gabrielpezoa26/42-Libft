c60b0284eb6f2bcb2e36bc1dbabb726815be4a5f
Gabriel <gabrielpezoa26@gmail.com>
2024-10-26 16:03:11 -0300

    fsdfsd

--- ft_striteri.c
+++ ft_striteri.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 12:38:28 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/26 14:11:44 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/26 16:02:13 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 #include <stdio.h>
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
@@ -18,9 +18,9 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	index;
 
 	index = 0;
-	while (i < ft_strlen(s))
+	while (index < ft_strlen(s))
 	{
-		f(i, s + i);
-		i++;
+		f(index, s + index);
+		index++;
 	}
 }
