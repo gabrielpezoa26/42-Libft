458325a99bffd6b25986111fef1cb5e8de97b2c3
Gabriel <gabrielpezoa26@gmail.com>
2024-10-26 19:55:58 -0300

    joinnnnn

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 12:38:28 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/26 19:36:06 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/26 19:51:38 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,28 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	size_t	size;
-	void	*dest_ptr;
-	char	*concat;
-	int		i;
+	size_t	i;
+	char	*dest_ptr;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	size = ft_strlen(s1) + ft_strlen(s2) + 1;
 	dest_ptr = malloc(size);
+	if (dest_ptr == NULL)
+		return (NULL);
 	i = 0;
-	*dest_ptr = s1;
-	i = 0;
-	while ()
-
-	concat = '\0';
+	while (*s1 != '\0')
+	{
+		dest_ptr[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		dest_ptr[i] = *s2;
+		i++;
+		s2++;
+	}
+	dest_ptr[i] = '\0';
 	return (dest_ptr);
 }
-
-int	main()
-{
-	const char	x[] = "aaa";
-	const char	y[] = "bbb";
-
-	printf("%s", ft_strjoin(x, y));
-}
-
-
-
-//	if (potato == NULL)
-//		return (NULL);
