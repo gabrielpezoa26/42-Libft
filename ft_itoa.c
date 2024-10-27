9b59a38c1be0ddb59b1a9f8dc1704c7bf221b4b0
Gabriel <gabrielpezoa26@gmail.com>
2024-10-27 14:58:14 -0300

    wrhgdhdgs

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,23 +6,33 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:46:09 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/27 14:33:33 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/27 14:54:49 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-	char	result;
+	int		result;
+	int		digit;
+	char	*y;
 
-	result = n + '0';
-	return (result);
+	if (n < 0)
+		n *= -1;
+	while (n > 9)
+	{
+		digit = n / 10;
+		//printf("%d", digit);
+	}
+	result = digit + '0';
+	y = (char *)result;
+	return (y);
 }
 
 int	main()
 {
 	int potato = 69;
 
-	printf("%c \n", ft_itoa(potato));
+	printf("%s \n", ft_itoa(potato));
 }
