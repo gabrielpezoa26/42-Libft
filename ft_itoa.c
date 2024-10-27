2c0aeaa9b6e594343545f69e2825f4c5913d23c6
Gabriel <gabrielpezoa26@gmail.com>
2024-10-26 22:55:10 -0300

    gfsgfs

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 12:38:28 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/26 22:41:07 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/26 22:55:05 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,5 @@
 
 char	*ft_itoa(int n)
 {
-	int	i;
-	int	rolex;
-	int	sign;
 
-	i = 0;
-	rolex = 0;
-	sign = 1;
 }
