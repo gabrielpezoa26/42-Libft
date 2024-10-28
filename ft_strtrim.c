25e2dbe4c6ea48332584e96545a26645029ffd88
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r2p2.42sp.org.br>
2024-10-28 13:58:34 -0300

    jksfjksdjkfd

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,22 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:55:07 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/28 13:32:40 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:56:38 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+
+static char	*sushi_de_tomate(const char *string, const char *galho_seco, size_t tamanho_da_poda)
+{
+	size_t	potato;
+	char	*after_poda;
+	
+	after_poda = ft_strnstr(string, galho_seco, tamanho_da_poda);
+	
+	return (after_poda);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -28,6 +38,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 			i++;
 		else if (position[i] != set[i])
 		{
+			sushi_de_tomate(s1, set, ft_strlen(s1));
 			result = ft_strdup(y);
 			i++;
 		}
