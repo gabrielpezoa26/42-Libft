d3a59afc407568acbdea4b47f157e889d6930d8f
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r2p2.42sp.org.br>
2024-10-28 17:32:36 -0300

    terminar trim

---
+++ errado.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errado.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 10:55:07 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/10/28 17:05:58 by gcesar-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+static char	*sushi(char const *str, char const *to_trim, size_t len)
+{
+	char	*after_trimming;
+
+	after_trimming = ft_strnstr(str, to_trim, len);
+	return (after_trimming);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*position;
+	char	*result;
+	int		i;
+	char	*y;
+
+	i = 0;
+	y = (char *)s1;
+	position = (char *)s1;
+	if (position == "")
+		return (NULL);
+	while (position[i] != '\0')
+	{
+		if (position[i] == set[i])
+			i++;
+		else if (position[i] != set[i])
+		{
+			result = sushi(s1, set, ft_strlen(s1));
+			result = ft_strdup(y);
+			i++;
+		}
+	}
+	return (result);
+}
+
+int main()
+{
+	char const	*beans = "-------hello--------";
+	char const	*designed_char = "-";
+
+	printf("%s\n", ft_strtrim(beans, designed_char));
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,50 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/28 10:55:07 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/28 15:29:14 by gcesar-n         ###   ########.fr       */
+/*   Created: 2024/10/28 15:52:20 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/10/28 15:54:06 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static char	*sushi(char const *str, char const *to_trim, size_t len)
-{
-	char	*after_trimming;
-
-	after_trimming = ft_strnstr(str, to_trim, len);
-	return (after_trimming);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*position;
-	char	*result;
-	int		i;
-	char	*y;
-
-	i = 0;
-	y = (char *)s1;
-	position = (char *)s1;
-	while (position[i] != '\0')
-	{
-		if (position[i] == set[i])
-			i++;
-		else if (position[i] != set[i])
-		{
-			result = sushi(s1, set, ft_strlen(s1));
-			result = ft_strdup(y);
-			i++;
-		}
-	}
-	return (result);
+	int	i;
+	int	j;
 }
-
-// int main()
-// {
-// 	char const	*beans = "-------hello--------";
-// 	char const	*designed_char = "-";
-
-// 	printf("%s\n", ft_strtrim(beans, designed_char));
-// }
