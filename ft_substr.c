3dca3ba65b6893f53ec54ef9c316cd92c0bac0fe
Gabriel <gabrielpezoa26@gmail.com>
2024-10-28 23:29:25 -0300

    francinette ok

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 11:51:55 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/10/26 21:31:42 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/10/28 23:24:35 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*potato;
+	size_t	s_len;
 
-	if (s == NULL)
-		return (0);
-	if (ft_strlen(s) < start)
-		len = 0;
-	if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (s_len - start < len)
+		len = s_len - start;
 	potato = malloc(sizeof(char) * (len + 1));
-	if (potato == NULL)
-		return (0);
+	if (!potato)
+		return (NULL);
 	ft_strlcpy(potato, s + start, len + 1);
 	return (potato);
 }
